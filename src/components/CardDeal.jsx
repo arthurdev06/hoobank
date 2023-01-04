03397c0b92619e884d058034221870d1003d297b
Arthur Rocha <116519847+arthurdev06@users.noreply.github.com>
2023-01-04 18:39:01 -0300

    Add files via upload

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../styles";
+import Button from "./Button";
+
+const CardDeal = () => (
+    <section className={layout.section}>
+        <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>
+                Find a better card deal <br className="sm:block hidden" /> in
+                few easy steps.
+            </h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                iste qui suscipit nostrum et voluptatum ipsum id alias, eaque
+                rem magni tempore maiores eveniet minima, aspernatur atque. Aut,
+                ducimus accusantium.
+            </p>
+            <Button styles="mt-10" />
+        </div>
+        <div className={layout.sectionImg}>
+            <img src={card} alt="card" className="w-[100%] h-[100%]" />
+        </div>
+    </section>
+);
+export default CardDeal;
